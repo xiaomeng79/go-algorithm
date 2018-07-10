eb789e8cc75fb3e94331eced27a72f1756b88947
xiaomeng79 <496096100@qq.com>
2018-07-10 10:50:45 +0800

    增加选择排序

--- bubble/bubble_test.go
+++ bubble/bubble_test.go
@@ -3,29 +3,19 @@ package bubble
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"github.com/xiaomeng79/go-algorithm/testdata"
 )
 
-var (
-	values = []struct {
-		nosort []int
-		sort   []int
-	}{
-		{[]int{3, 44, 56, 38, 77, 38, 26}, []int{3, 26, 38, 38, 44, 56, 77}},
-		{[]int{3}, []int{3}},
-		{[]int{3, -1, -6, 34, -78}, []int{-78, -6, -1, 3, 34}},
-	}
-	value = []int{3, 44, 56, 38, 77, 38, 26}
-)
 
 func TestBubbleSort(t *testing.T) {
-	for _, v := range values {
-		assert.Exactly(t, v.sort, BubbleSort(v.nosort), "no eq")
+	for _, v := range testdata.Values {
+		assert.Exactly(t, v.Sort, BubbleSort(v.Nosort), "no eq")
 	}
 }
 
 func BenchmarkBubbleSort(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		BubbleSort(value)
+		BubbleSort(testdata.Value)
 	}
 }
---
+++ selection/selection.go
@@ -0,0 +1,28 @@
+//选择排序
+package selection
+
+//原理：每一次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置
+//稳定: 不稳定
+//适用: 数据规模越小越好
+//算法步骤:
+//1. 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+//2. 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾
+//3. 重复第二步，直到所有元素均排序完毕
+
+func SelectionSort(arr []int) []int {
+	length := len(arr)
+	for i:= 0;i < length-1;i++ {//注意这里使用length-1少一次，
+		minIndex := i
+		for j:= i+1;j<length;j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+		//
+		arr[minIndex],arr[i] = arr[i],arr[minIndex]
+	}
+	return arr
+}
+
+
+
